a0bba9bbcde6ce3df6f20a862706596aa2cd4af5
Miracle Timothy <miracletimothyofficial@gmail.com>
2026-01-11 13:07:43 +0300

    Setup server and endpoint to create payment intent

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import Stripe from "stripe";
+const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY));
+
+// const customer = await stripe.customers.create({
+//   email: "miracletimothy@gmail.com",
+// });
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req: Request, res: Response) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 2000,
+    currency: "usd",
+    automatic_payment_methods: { enabled: true },
+  });
+
+  res.json({ clientSecret: paymentIntent.client_secret });
+});
+
+app.listen(5000, "192.168.78.4", () => {
+  console.log("server is running");
+});
